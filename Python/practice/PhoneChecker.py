951a6015fb2479607353d68a3b999dc0bfaed3b7
Ethan Buranday <buranday.ethan@gmail.com>
2019-09-02 01:09:53 -0700

    Added menu for the user to choose which function to use

--- Python/practice/PhoneChecker.py
+++ Python/practice/PhoneChecker.py
@@ -3,63 +3,196 @@ NOTE: THIS CHECKER IS STILL IN PROGRESS. ALGORITHIMS FOR COMPLEX NUMBERS NOT IMP
 
 """
 import re
+import sys
+import time
+
+
+# STARTUP
+def main():
+    warning = print(""" 
+
+        ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗     ██████╗ ██╗  ██╗ ██████╗ ███╗   ██╗███████╗     ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗███████╗██████╗ 
+        ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗    ██╔══██╗██║  ██║██╔═══██╗████╗  ██║██╔════╝    ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+        ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║    ██████╔╝███████║██║   ██║██╔██╗ ██║█████╗      ██║     ███████║█████╗  ██║     █████╔╝ █████╗  ██████╔╝
+        ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║    ██╔═══╝ ██╔══██║██║   ██║██║╚██╗██║██╔══╝      ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+        ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝    ██║     ██║  ██║╚██████╔╝██║ ╚████║███████╗    ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗███████╗██║  ██║
+         ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝     ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                                                                                                                                                                                
+    """)
+    
+    time.sleep(2)
+
+    print("""
+    NOTE: THIS PROGRAM IS A WORK IN PROGRESS! SOME PHONE NUMBERS MIGHT NOT WORK. I AM CURRENTLY WORKING ON ADDING EXTENSION NUMBERS AND OTHER STUFFS. PLEASE CONTACT ME FOR
+          SUGGESSTIONS AT BURANDAY.ETHAN@GMAIL.COM OR THROUGH GITHUB.
+    """)
+
+def continue_to_program():
+
+    proceed = input("""
+        WOULD YOU LIKE TO PROCEED?
+        1: YES
+        2: NO
+        
+        ENTER CHOICE HERE: """)
+
+    if proceed == "1":
+        menu()
+    elif proceed == "2":
+        print("""
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        EXISTING THE PROGRAM
+        
+        
+
+
+
+
+
+
+
+
+
+
+        """)
+        time.sleep(1)
+        quit()
+    elif proceed == "" or proceed == " " :
+        print("""
+        
+        
+        TRY AGAIN!
+        
+        
+        """)
+        continue_to_program()
+
+
+
+# MENU 
+def menu():
+    
+    choice = input("""
+
+    WELCOME TO PHONE CHECKER")
+    Please select the following choices below: 
+    1: PHONE CHECKER
+    2: MESSAGE SCANNER
+    3: AREA CODE DETECTOR
+    
+    ENTER CHOICE HERE: """)
+
+    if choice == "1":
+        isPhoneNumber()
+    elif choice == "2":
+        isMessage()
+    elif choice == "3":
+        isAreaCode()
+    elif choice == "" or choice == " ":
+        print("""
+        
+        CHOICE NOT VALID, PLEASE ENTER VALID CHOICE!
+        
+        """)
+        menu()
+    else:
+        print("""
+        
+        CHOICE NOT VALID, PLEASE ENTER VALID CHOICE!
+        
+        """)
+        menu()
+        
 
 
 
 
 # INPUTS
-phone_number = input("Enter a phone number: ")
-message = input("Enter a message to identify the phone number(s): ")
-area_code = input("Enter a phone number to detect the area code: ")
+# phone_number = input("Enter a phone number: ")
+# message = input("Enter a message to identify the phone number(s): ")
+# area_code = input("Enter a phone number to detect the area code: ")
 
 
 # PHONE NUMBER CHECKER
-def isPhoneNumber(phone_number):
-    """""""""
-    Checks the format of the phone number.
-    Scans the first three, second three, and last four digits and uses .isdecimal() function if the string contains decimals.
-    Check hyphens in phone_number 3 and 7.
-    """""""""
-    if len(phone_number) != 12:
-        return False
-    for i in range (0, 3):
-        if not phone_number[i].isdecimal():
-            return False
-    if phone_number[3] != '-':
-        return False
-    for i in range(4, 7):
-        if not phone_number[i].isdecimal():
-            return False
-    if phone_number[7] != '-':
-        return False
-    for i in range(8, 12):
-        if not phone_number[i].isdecimal():
-            return False
-    return True
+def isPhoneNumber():
+    # """""""""
+    # Checks the format of the phone number.
+    # Scans the first three, second three, and last four digits and uses .isdecimal() function elif the string contains decimals.
+    # Check hyphens in phone_number 3 and 7.
+    # """""""""
+
+
+    # """
+    # ADD ART FOR THIS FUNCTION
+    # """
+
+    phone_number = input("Enter a phone number: ")
+
+    """
+    REWORK ON PHONE NUMBER AND ADD REGEX INSTEAD OF IF STATEMENTS
+    """
+
+    # if len(phone_number) != 12:
+    #     return False
+    # for i in range (0, 3):
+    #     if not phone_number[i].isdecimal():
+    #         return False
+    # if phone_number[3] != '-':
+    #     return False
+    # for i in range(4, 7):
+    #     if not phone_number[i].isdecimal():
+    #         return False
+    # if phone_number[7] != '-':
+    #     return False
+    # for i in range(8, 12):
+    #     if not phone_number[i].isdecimal():
+    #         return False
+    # return True
 
 # MESSAGE SCANNER
-def isMessage(message):
-    """""""""""
-    Detects any phone number(s) that are in the message.
-    """""""""""
-    for i in range(len(message)):
-        chunk = message[i:i+12]
-        if isPhoneNumber(chunk):
-            return("Phone number(s) found: " + str(chunk))
-        else:
-            return("No phone number(s) detected.")
+# def isMessage():
+#     """""""""""
+#     Detects any phone number(s) that are in the message.
+#     """""""""""
+#     message = input("Enter a message to identify the phone number(s): ")
+#     for i in range(len(message)):
+#         chunk = message[i:i+12]
+#         elif isPhoneNumber(chunk):
+#             return("Phone number(s) found: " + str(chunk))
+#         else:
+#             return("No phone number(s) detected.")
 
-# AREA CODE IDENTIFIER
-def isAreaCode(area_code):
-    phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-    area_code = phoneNumRegex.search(area_code)   
-    return("Area code detected: " + area_code.group(1))
+# # AREA CODE IDENTIFIER
+# def isAreaCode():
+#     area_code = input("Enter a phone number to detect the area code: ")
+#     phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+#     area_code = phoneNumRegex.search(area_code)   
+#     return("Area code detected: " + area_code.group(1))
 
 
 # OUTPUTS
-"""""""""""
-Prints the output
-"""""""""""
-print(phone_number + ' is a phone number: ' + str(isPhoneNumber(phone_number)))
-print(isMessage(message))
-print(isAreaCode(area_code))
+# """""""""""
+# Prints the output
+# """""""""""
+# print(phone_number + ' is a phone number: ' + str(isPhoneNumber(phone_number)))
+# print(isMessage(message))
+# print(isAreaCode(area_code))
+
+
+main()
+continue_to_program()
+# isPhoneNumber()
+
+
+
